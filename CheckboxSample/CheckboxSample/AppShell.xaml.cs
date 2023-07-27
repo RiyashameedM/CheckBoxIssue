4f73653fbc8f9d716d9775cea444e6a6a2718bd6
riyas <riyashameed.m@syncfusion.com>
2023-07-27 15:30:10 +0530

    Checkbox sample

---
+++ CheckboxSample/CheckboxSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CheckboxSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CheckboxSample/CheckboxSample/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CheckboxSample;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+	{
+
+	}
+}
+
